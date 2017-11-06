b25f2fe98cad3877d294154375f9f4deb0e8d3c9
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-06 14:28:59 -0500

    Checks and inserts quarters, dimes, nickels and pennies accordingly. Passes 6 tests

--- changer.rb
+++ changer.rb
@@ -1,10 +1,41 @@
 class Changer
   def self.make_change(num)
     change_array = []
-    if num % 25 == 0
-      change_array << 25
+
+    while num > 0
+      if (num % 25) != num
+        factor = num / 25
+        factor.times do
+          change_array << 25
+          num -= 25
+        end
+      elsif (num % 10) != num
+        factor = num / 10
+        factor.times do
+          change_array << 10
+          num -= 10
+        end
+      elsif (num % 5) != num
+        factor = num / 5
+        factor.times do
+          change_array << 5
+          num -= 5
+        end
+      else
+        num.times do
+          change_array << 1
+          num -= 1
+        end
+      end
     end
 
     return change_array
   end
 end
+
+# check if divible by 25
+# check how many times divisible by 25
+# append 25 to array, as many times as original number is divisible by 25
+# check if remainder is divisble by 10
+# check how many times divisible by 10
+# etc ...
